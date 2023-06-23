686c58f591b5d007936b622d2ce1a0d0144f9e70
anikdhar <aniksutradhar11@gmail.com>
2023-06-23 20:18:34 +0600

    file setup

---
+++ musical-pro/src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Herosection from "../Herosection/Herosection";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Herosection></Herosection>
+    </div>
+  );
+};
+
+export default Home;
